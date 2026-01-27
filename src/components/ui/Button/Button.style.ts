82c10173b9fd700f93da9695ab9da0853f39708a
edemit <shuldeshovwork013@gmail.com>
2026-01-27 10:54:07 +0100

    Adding styles for buttons

--- src/components/ui/Button/Button.style.ts
+++ src/components/ui/Button/Button.style.ts
@@ -1,24 +1,24 @@
-import { Button, ButtonProps } from '@chakra-ui/react';
-import styled, { css } from 'styled-components';
+import { Button, ButtonProps } from "@chakra-ui/react";
+import styled, { css } from "styled-components";
 
 export const StyledButton = styled(Button)<ButtonProps>`
-    width: 200px;
-    height: 40px;
-    color: #ffffff;
-    background-color: #D00039;
-    border-radius: 20px;
-    border: 1px solid #000000;
-    font-family: Georgia;
-    font-style: italic;
-    font-weight: bold;
+  width: 200px;
+  height: 40px;
+  color: #ffffff;
+  background-color: #d00039;
+  border-radius: 20px;
+  border: 1px solid #000000;
+  font-family: Georgia;
+  font-style: italic;
+  font-weight: bold;
 
-    ${(props) => 
-        props.variant === 'outline' && css`
-        color: #000000;
-        background-color: #FFFFFF;
-        border: 1px solid #E1E8F1;
-        font-style: normal;
-        font-weight: normal;
-        `
-    }
-`
+  ${(props) =>
+    props.variant === "outline" &&
+    css`
+      color: #000000;
+      background-color: #ffffff;
+      border: 1px solid #e1e8f1;
+      font-style: normal;
+      font-weight: normal;
+    `}
+`;
